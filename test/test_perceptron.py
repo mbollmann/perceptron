eb50464802e05b95695ab51f969bd0a82cfcbfb5
Marcel Bollmann <bollmann@linguistics.rub.de>
2015-11-05 17:54:13 +0100

    Add triple-classes test

--- test/test_perceptron.py
+++ test/test_perceptron.py
@@ -22,10 +22,35 @@ class TestCombinatorialPerceptron(object):
              np.array([0,1,1]),
              np.array([1,0,1]),
              np.array([1,1,1])]
-        y = [0, 0, 0, 1]
+        y = np.array([0, 0, 0, 1])
         p = CombinatorialPerceptron(iterations=100)
         p.train(x, y)
         assert p.predict_vector(np.array([0,1,1])) == 0
         assert p.predict_vector(np.array([1,0,1])) == 0
         assert p.predict_vector(np.array([1,1,1])) == 1
         assert p.predict_vector(np.array([0,0,1])) == 0
+
+    def test_three_classes(self):
+        x = [np.array([0,0,0,0,1]),
+             np.array([0,1,0,0,1]),
+             np.array([0,0,0,1,1]),
+             np.array([0,1,0,1,1]),
+             np.array([1,0,0,0,1]),
+             np.array([1,0,0,1,1]),
+             np.array([1,0,1,0,1]),
+             np.array([0,0,1,0,1]),
+             np.array([0,1,1,0,1])
+             ]
+        y = np.array([0,0,0,0,1,1,2,2,2])
+        p = CombinatorialPerceptron(iterations=100)
+        p.train(x, y)
+        assert p.predict_vector(np.array([0,0,0,0,1])) == 0
+        assert p.predict_vector(np.array([0,0,0,1,1])) == 0
+        assert p.predict_vector(np.array([0,1,0,0,1])) == 0
+        assert p.predict_vector(np.array([1,0,0,0,1])) == 1
+        assert p.predict_vector(np.array([0,0,1,0,1])) == 2
+        assert p.predict_vector(np.array([0,1,0,1,1])) == 0
+        assert p.predict_vector(np.array([1,0,0,1,1])) == 1
+        assert p.predict_vector(np.array([0,1,1,0,1])) == 2
+        assert p.predict_vector(np.array([1,0,1,0,1])) == 2
+        assert p.predict_vector(np.array([1,1,1,1,1])) == 2
