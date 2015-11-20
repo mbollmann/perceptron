ae854a32fd32928807bde68955d310f94672dd57
Marcel Bollmann <bollmann@linguistics.rub.de>
2015-11-20 18:29:26 +0100

    Simplify Viterbi perceptron by making it inherit from standard one

--- mmb_perceptron/mixed_impl/combinatorial_viterbi_perceptron.py
+++ mmb_perceptron/mixed_impl/combinatorial_viterbi_perceptron.py
@@ -3,43 +3,24 @@
 import itertools as it
 import numpy as np
 import operator as op
-from ..perceptron import Perceptron
+from .combinatorial_perceptron import CombinatorialPerceptron_Mixed
 
-class defaultdict_numpy(dict):
-    def __init__(self, dim):
-        self._dim = dim
-
-    def __missing__(self, key):
-        return np.zeros(self._dim)
-
-    def copy(self):
-        obj = defaultdict_numpy(self._dim)
-        for (key, value) in self.iteritems():
-            obj[key] = value.copy()
-        return obj
-
-class CombinatorialViterbiPerceptron_Mixed(Perceptron):
+class CombinatorialViterbiPerceptron_Mixed(CombinatorialPerceptron_Mixed):
     """Perceptron classifier with combinatorial feature explosion and viterbi
     decoding.
 
     Terribly slow, probably shouldn't be used.
     """
-
     branch_limit = 8
+    sequenced = True
 
-    @property
-    def all_labels(self):
-        try:
-            return self._all_labels
-        except AttributeError:
-            self._all_labels = sorted(self._label_mapper)
-            return self._all_labels
+    def predict_features(self, features):
+        raise NotImplementedError("predict_features cannot be used with "
+                                  "Viterbi decoding")
 
-    def reset_weights(self):
-        self._w = defaultdict_numpy(len(self._label_mapper))
-
-    def _resize_weights(self, w):
-        pass
+    def _perform_train_iteration_independent(self, x, y, permutation):
+        raise NotImplementedError("independent prediction cannot be used with "
+                                  "Viterbi decoding")
 
     def predict(self, x):
         """Predict the class label of a given data point.
@@ -80,16 +61,6 @@ class CombinatorialViterbiPerceptron_Mixed(Perceptron):
         guesses = best_path[self._left_context_size:]
         return guesses
 
-    def _preprocess_data(self, data):
-        """Preprocess a full list of training data.
-        """
-        if len(data) < 1:
-            self.feature_count = 0
-            return data
-        self._feature_extractor.init(data)
-        data = [self._feature_extractor.get(x) for x in data]
-        return data
-
     def _preprocess_train(self, x, y):
         assert len(x) == len(y)
         self._label_mapper.reset()
@@ -98,33 +69,6 @@ class CombinatorialViterbiPerceptron_Mixed(Perceptron):
         for seq_y in y:
             self._label_mapper.extend(seq_y)
         return (new_x, y)
-
-    def average_weights(self, all_w):
-        averaged = defaultdict_numpy(len(self._label_mapper))
-        divisor = float(len(all_w))
-        final_w = all_w[-1]
-        for feat, label_weights in final_w.iteritems():
-            averaged[feat] = sum((_w[feat] for _w in all_w)) / divisor
-        return averaged
-
-    def print_weights(self):
-        all_labels = self.all_labels
-        # header
-        print("\t" + "\t".join(all_labels).encode("utf-8"))
-        sorted_indices = self._label_mapper.map_list(all_labels)
-        # feature weights
-        for feature, label_weights in self._w.iteritems():
-            row = [feature]
-            row.extend(label_weights[sorted_indices])
-            print("\t".join(map(unicode, row)).encode("utf-8"))
-
-    def prune_weights(self):
-        prunable = []
-        for feature, label_weights in self._w.iteritems():
-            if all((abs(w) < self.prune_limit for w in label_weights)):
-                prunable.append(feature)
-        for f in prunable:
-            del self._w[f]
 
     ############################################################################
     #### Sequenced prediction ##################################################
